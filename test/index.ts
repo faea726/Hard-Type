7bfb9789c66a6302ec68ad1cc62148e87aa3121f
faea726 <fantasy.eagle96@gmail.com>
2022-05-16 20:14:47 +0700

    Update test

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,49 @@
+import { Provider } from "@ethersproject/abstract-provider";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Tester } from "../typechain";
+import { Wallet } from "ethers"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Contract", function () {
+  var Contract;
+  var ctr: Tester;
+  var accounts: SignerWithAddress[];
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  this.beforeAll(async () => {
+    Contract = await ethers.getContractFactory("Tester");
+    ctr = await Contract.deploy("Contract", "CTR", 9, 100000000);
+    await ctr.deployed();
+    accounts = await ethers.getSigners();
+    console.log("   Contract deployed by owner:", accounts[0].address);
+  });
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  describe("Queries:", () => {
+    it("Token infors", async () => {
+      const name = await ctr.name();
+      const symbol = await ctr.symbol();
+      const decimals = await ctr.decimals();
+      const totalSupply = await ctr.totalSupply();
+      console.log("       Name:", name);
+      console.log("       Symbol:", symbol);
+      console.log("       Decimals:", decimals);
+      console.log("       Total Supply:", (totalSupply.div(10 ** decimals)).toString());
+    });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    it("True owner", async () => {
+      expect(await ctr.balanceOf(accounts[0].address)).to.equal(await ctr.totalSupply())
+    });
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    it("Transfer", async () => {
+      const transferAmount = 100;
+
+      await ctr.transfer(accounts[2].address, transferAmount);
+      expect((await ctr.balanceOf(accounts[2].address)).toNumber()).to.equal(transferAmount);
+
+      await ctr.connect(accounts[2]).transfer(accounts[0].address, transferAmount);
+      expect((await ctr.balanceOf(accounts[2].address)).toNumber()).to.equal(0);
+
+      await expect(ctr.connect(accounts[1]).transfer(accounts[2].address, 100)).to.be.reverted;
+    });
   });
 });
